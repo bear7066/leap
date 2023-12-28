4a9e13cb420ce4ec376187f90c189a1b663e8206
Harry <kidflash7011@gmail.com>
2023-12-28 10:48:14 +0800

    interactionBox bug

--- part4/script.js
+++ part4/script.js
@@ -4,8 +4,16 @@
 //     if(frame.hands.length > 0) {
 //         var hand = frame.hands[0];
 //         var output = document.getElementById('output');
+//         var interactionBox = frame.interactionBox;
+//         if(interactionBox){
+//             console.log("nice");
+//         }else{
+//             console.log("fork!");
+//         }
+        
 //         output.innerHTML = 'Hand detected. Palm position: ' 
 //                            + hand.palmPosition.join(', ');
+        
 //     }
 // });
 
@@ -27,18 +35,45 @@
 //     }
 // });
 
-// test
-Leap.loop(function(frame) {
-    if (frame.pointables.length > 0) {
-        var pointable = frame.pointables[0];
+// test 2: interactionBox
+
+// Leap.loop(function(frame) {
+//     if (frame.pointables.length > 0) {
+//         var pointable = frame.pointables[0];
+//         var interactionBox = frame.interactionBox;
+
+//         if (interactionBox) {
+//             var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
+
+//             // 接下來的程式碼...
+//         } else {
+//             console.error('Interaction box is undefined.');
+//         }
+//     }
+// });
+
+
+// test 3: find that interactionBox is unavailable
+var controller = new Leap.Controller();
+
+controller.on("frame", function(frame) {
+    var output = document.getElementById('output');
+
+    if(frame.hands.length > 0) {
+        var hand = frame.hands[0];
         var interactionBox = frame.interactionBox;
 
-        if (interactionBox) {
-            var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
-
-            // 接下來的程式碼...
+        if(interactionBox){
+            output.innerHTML = 'Interaction box available';
         } else {
-            console.error('Interaction box is undefined.');
+            output.innerHTML = 'Interaction box available';
         }
+
+        output.innerHTML = 'Hand detected. Palm position: ' 
+                           + hand.palmPosition.join(', ');
+    } else {
+        output.innerHTML = 'Waiting for hand detection...';
     }
 });
+
+controller.connect();
